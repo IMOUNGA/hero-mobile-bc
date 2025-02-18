6d95745dc006161c2fe00d72b1d0ad5729ba678a
romann <imounga.r@gmail.com>
2025-02-18 08:37:45 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('Test git init');
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
