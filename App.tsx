5f6d5050c540d124aedc8afda9fc8f4858cbb27a
romann <imounga.r@gmail.com>
2025-02-18 08:47:17 +0100

    page structure

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {ImageBackground, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function App() {
-  console.log('Test git init');
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    return (
+        <SafeAreaProvider style={styles.appContainer}>
+            <ImageBackground
+                source={require('./assets/Background 390x908.png')}
+                resizeMode={'cover'}
+                style={styles.background}>
+                <SafeAreaView style={styles.safeArea}>
+                    <View style={styles.container}>
+                        <Text>Open up App.tsx to start working on your app!</Text>
+                        <StatusBar style="auto"/>
+                    </View>
+                </SafeAreaView>
+            </ImageBackground>
+        </SafeAreaProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: {
+        flex: 1,
+    },
+    safeArea: {
+        flex: 1,
+    },
+    background: {
+        flex: 1,
+        width: '100%',
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
 });
